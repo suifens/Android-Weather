1858df5bc5a84a5aaa52403971e565f85f646d05
zhengyixiong <zhengyixiong@gengee.cn>
2020-05-25 23:39:25 +0800

    [Feature] 添加字体

---
+++ app/src/main/java/com/goodtech/tq/utils/font/AlternateBoldTextView.java
@@ -0,0 +1,28 @@
+package com.goodtech.tq.utils.font;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+public class AlternateBoldTextView extends AppCompatTextView {
+    public AlternateBoldTextView(Context context) {
+        super(context);
+    }
+
+    public AlternateBoldTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public AlternateBoldTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void init(Context context){
+        AssetManager assetManager = context.getAssets();
+        Typeface fontType = Typeface.createFromAsset(assetManager,"fonts/DINAlternate-Bold.ttf");
+        setTypeface(fontType);
+    }
+}
