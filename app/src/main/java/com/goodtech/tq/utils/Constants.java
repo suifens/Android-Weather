c8fc1c94d272b2e048c733b9159f006413608b79
suifens <zyxde1991@163.com>
2021-11-05 15:45:38 +0800

    [Feature] 5.1.1

--- app/src/main/java/com/goodtech/tq/utils/Constants.java
+++ app/src/main/java/com/goodtech/tq/utils/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String APP_ID = "1106354620"; //应用ID
     public static final String SPLASH_POS_ID = "9000928561858848";  //广告id
     public static final String BANNER_POS_ID = "9001366014022230";  //banner id
-    public static final String EXPRESS_POS_ID = "3091019114002285";  //动态信息流 id
+    public static final String EXPRESS_POS_ID = "9052754449381091";  //动态信息流 id
     public static final String NEWS_POS_ID = "7081917104907407";  //上文下图信息流 id
 
     public static final String UM_APP_ID = "595a3b236e27a468630021e8";
